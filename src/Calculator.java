f88051e5f187474bd101c47b3c3378c8378eff43
Ankur Sangwan <ankursangwan0106@gmail.com>
2025-07-09 12:07:41 +0530

    new line added

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,6 +3,7 @@ public class Calculator {
         System.out.println("-------------");
         System.out.println("its me !! git");
         System.out.println("hy !! stay away from me");
+        System.out.println("Ankur is smart");
     }
 
 
